41b16d78f07263249294e49f85d93e017c74e599
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-12 00:00:00 -0300

    Requisições (GET e POST)

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        return HttpResponse(confirmar_senha)
 
 
 def logar(request):
