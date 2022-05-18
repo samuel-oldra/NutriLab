1707751ecdebe3cc852e216a3c1cdcfe2a919a30
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-18 00:00:00 -0300

    Logar e deslogar do sistema

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name="cadastro"),
-    path('logar/', views.logar, name="logar")
+    path('logar/', views.logar, name="logar"),
+    path('sair/', views.sair, name="sair"),
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
@@ -9,6 +10,8 @@ from .utils import password_is_valid
 
 def cadastro(request):
     if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'cadastro.html')
     elif request.method == "POST":
         username = request.POST.get('usuario')
@@ -34,4 +37,25 @@ def cadastro(request):
 
 
 def logar(request):
-    return HttpResponse("Você está na página de login")
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'logar.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+
+        usuario = auth.authenticate(username=username, password=senha)
+
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect('/auth/logar')
+        else:
+            auth.login(request, usuario)
+
+    return redirect('/')
+
+
+def sair(request):
+    auth.logout(request)
+    return redirect('/auth/logar')
