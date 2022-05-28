f1620eb7ae02d218f01af6d3021e31f21357ca90
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-28 00:00:00 -0300

    Menu

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 @login_required(login_url='/auth/logar/')
 def pacientes(request):
-    return HttpResponse('Olá')
+    return render(request, 'pacientes.html')
