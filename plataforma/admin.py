0d115cf590158f4e25149a93d7c6c94a2ed20985
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-06-20 00:00:00 -0300

    Inclui models na admin (Refeicao e Opcao)

--- plataforma/admin.py
+++ plataforma/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Pacientes, DadosPaciente
+from .models import Pacientes, DadosPaciente, Refeicao, Opcao
 
 admin.site.register(Pacientes)
 admin.site.register(DadosPaciente)
+admin.site.register(Refeicao)
+admin.site.register(Opcao)
