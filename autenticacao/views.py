5e9f92b2cc583b86cec69aaa62773b6d948a0a70
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-07-10 19:00:00 -0300

    Rotas

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def cadastro(request):
+    return HttpResponse("Você está na página de cadastro")
+
+
+def logar(request):
+    return HttpResponse("Você está na página de login")
