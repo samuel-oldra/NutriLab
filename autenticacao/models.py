cfc6e981c85fa3dd486657eca6efa1abd6ad0ff8
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-07-19 19:00:00 -0300

    CMD: (env) python manage.py makemigrations

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Ativacao(models.Model):
+    token = models.CharField(max_length=64)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    ativo = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
