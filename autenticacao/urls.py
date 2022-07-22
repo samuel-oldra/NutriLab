4df4138789ac2fffe3dff21f921cdf5d25b66837
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-07-22 19:00:00 -0300

    Ativação do usuário (link por e-mail)

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cadastro/', views.cadastro, name="cadastro"),
     path('logar/', views.logar, name="logar"),
     path('sair/', views.sair, name="sair"),
+    path('ativar_conta/<str:token>/', views.ativar_conta, name="ativar_conta"),
 ]
