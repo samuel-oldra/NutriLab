d5b5c7f5744c531fe8d571861eb905c1055b5a97
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-07-27 19:00:00 -0300

    Verifica usuário logado com 'decorators'

--- nutrilab/urls.py
+++ nutrilab/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('autenticacao.urls')),
+    path('', include('plataforma.urls')),
 ]
