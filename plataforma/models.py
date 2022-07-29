f48764a38d09f33b53d31e9695c6b8d23610eb92
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-07-29 19:00:00 -0300

    CMD: (env) python manage.py makemigrations

--- plataforma/models.py
+++ plataforma/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Pacientes(models.Model):
+    choices_sexo = (('F', 'Feminino'),
+                    ('M', 'Maculino'))
+    nome = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=1, choices=choices_sexo)
+    idade = models.IntegerField()
+    email = models.EmailField()
+    telefone = models.CharField(max_length=19)
+    nutri = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+def __str__(self):
+    return self.nome
