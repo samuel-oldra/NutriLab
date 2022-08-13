86f40176b17e4da2891529be9771bb5f65a85d04
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-08-13 19:00:00 -0300

    Carrega dados do paciente (Gráfico)

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('pacientes/', views.pacientes, name='pacientes'),
     path('dados_paciente/', views.dados_paciente_listar, name="dados_paciente_listar"),
     path('dados_paciente/<str:id>/', views.dados_paciente, name="dados_paciente"),
+    path('grafico_peso/<str:id>/', views.grafico_peso, name="grafico_peso"),
 ]
--- plataforma/views.py
+++ plataforma/views.py
@@ -2,7 +2,9 @@ from datetime import datetime
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages import constants
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Pacientes, DadosPaciente
 
@@ -81,3 +83,14 @@ def dados_paciente(request, id):
 
         messages.add_message(request, constants.SUCCESS, 'Dados cadastrado com sucesso')
         return redirect('/dados_paciente/')
+
+
+@login_required(login_url='/auth/logar/')
+@csrf_exempt
+def grafico_peso(request, id):
+    paciente = Pacientes.objects.get(id=id)
+    dados = DadosPaciente.objects.filter(paciente=paciente).order_by("data")
+    pesos = [dado.peso for dado in dados]
+    labels = list(range(len(pesos)))
+    data = {'peso': pesos, 'labels': labels}
+    return JsonResponse(data)
