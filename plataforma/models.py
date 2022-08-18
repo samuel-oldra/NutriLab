4bcc86f01b2b3af7d910965f1401a5a4982f6e9c
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-08-18 19:00:00 -0300

    Adicionados models para refeição e opção

--- plataforma/models.py
+++ plataforma/models.py
@@ -30,3 +30,24 @@ class DadosPaciente(models.Model):
 
     def __str__(self):
         return f"Paciente({self.paciente.nome}, {self.peso})"
+
+
+class Refeicao(models.Model):
+    paciente = models.ForeignKey(Pacientes, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=50)
+    horario = models.TimeField()
+    carboidratos = models.IntegerField()
+    proteinas = models.IntegerField()
+    gorduras = models.IntegerField()
+
+    def __str__(self):
+        return self.titulo
+
+
+class Opcao(models.Model):
+    refeicao = models.ForeignKey(Refeicao, on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to="opcao")
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.descricao
