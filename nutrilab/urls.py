3b71c727f54889c74b820905dd8f0d9b785bbc2a
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-08-23 19:00:00 -0300

    Cadastro e listagem das opções

--- nutrilab/urls.py
+++ nutrilab/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,3 +23,5 @@ urlpatterns = [
     path('auth/', include('autenticacao.urls')),
     path('', include('plataforma.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
